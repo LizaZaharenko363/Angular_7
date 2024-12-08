e2246a8b0fb4b2a7ccb532210a2d1af1f4865691
LizaZaharenko363 <liza.zaharenko25@gmail.com>
2024-12-08 14:17:44 +0200

    added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ComponentOneComponent } from './component-one/component-one.component';
+import { ComponentTwoComponent } from './component-two/component-two.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    ComponentTwoComponent,
+    ComponentOneComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
